7b951c980ca29b1281bdebe330adbefaa462d947
kaushik <joshikaushik419@gmail.com>
2024-08-22 10:40:27 +0530

    added java programs

---
+++ Person.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Person{
+	String fname,lname,mname;
+
+	public Person(String fname,String mname,String lname){
+		this.fname=fname;
+		this.mname=mname;
+		this.lname=lname;
+	}
+
+	public String toString(){
+		String fnamefirst=fname.substring(0,1);
+		String fremainingletters=fname.substring(1,fname.length());
+		fnamefirst=fnamefirst.toUpperCase();
+		fname=fnamefirst+fremainingletters;
+
+		String mnamefirst=mname.substring(0,1);
+		String mremainingletters=mname.substring(1,mname.length());
+		mnamefirst=mnamefirst.toUpperCase();
+		mname=mnamefirst+mremainingletters;
+		
+		String lnamefirst=lname.substring(0,1);
+		String lremainingletters=lname.substring(1,lname.length());
+		lnamefirst=lnamefirst.toUpperCase();
+		lname=lnamefirst+lremainingletters;
+
+		return lname+" "+fname+" "+mname;
+	}
+
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter firstname, Middlename, and Lastname of a person");
+		String f=s.next();
+		String m=s.next();
+		String l=s.next();
+		Person p=new Person(f,m,l);
+		System.out.println(p);
+	}
+}
+
+
--- Student.java
+++ Student.java
@@ -1,24 +1,70 @@
+import java.io.*;
 class Student{
-	int rno;
-	String name;
-	static String classTeacher;
+	private int rollno;
+	private String sname;
+	private float per;
+	private static int cnt=0;
 	
-	Student(int r,String n){
-		rno=r;
-		name=n;
+	public Student(int r,String n,float p){
+		rollno=r;
+		sname=n;
+		per=p;
+		cnt++;
 	}
-	static void assignTeacher(String name){
-		classTeacher =name;
-	}
+
 	public String toString(){
-		return "["+rno+","+name+","+classTeacher+"]";
+		return "roll no: "+rollno+" Name: "+sname+" Percentage: "+per;
 	}
-	public static void main(String args[]){
-		Student s1=new Student(1,"a");
-		Student s2=new Student(2,"b");
-		Student.assignTeacher("ABC");
-		System.out.println(s1);
-		System.out.println(s2);
+
+	public static void sort(Student s[]){
+		int i,j;
+		String temp;
+		int t1;
+		float t2;
+		for(i=0;i<s.length;i++){
+			for(j=i+1;j<s.length;j++){
+				if(s[i].per>s[j].per){
+					temp=s[i].sname;
+					s[i].sname=s[j].sname;
+					s[j].sname=temp;
+
+					t1=s[i].rollno;
+					s[i].rollno=s[j].rollno;
+					s[j].rollno=t1;
+
+					t2=s[i].per;
+					s[i].per=s[j].per;
+					s[j].per=t2;
+				}
+			}
+		}
+	}
+
+	public static void main(String args[]) throws IOException{
+		int rno=1;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter how many Students");
+		int n=Integer.parseInt(br.readLine());
+		Student[] s=new Student[n];
+		System.out.println("No of objects= "+Student.cnt);
+		for(int i=0;i<n;i++){
+			System.out.println("\nEnter the Name:");
+			String sname=br.readLine();
+			System.out.println("\nEnter the Percentage");
+			float per=Float.parseFloat(br.readLine());
+			s[i]=new Student(rno,sname,per);rno++;
+		}
+		System.out.println("No. of objects: "+Student.cnt);
+		for(int i=0;i<n;i++){
+			sort(s);
+			System.out.println("\n*******SORTED DETAILS ARE*******");
+			for(i=0;i<n;i++){
+				System.out.println(s[i]);
+			}
+		}
 	}
 }
+
+	
+
 
