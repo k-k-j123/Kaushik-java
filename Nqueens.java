2964a149a1ce06a61c32c4217b81cee38ab4b45e
kaushik <joshikaushik419@gmail.com>
2024-08-29 20:19:20 +0530

    added Nqueens and sudoku solver

---
+++ Nqueens.java
@@ -0,0 +1,61 @@
+class Nqueens{
+	public static boolean isSafe(char board[][],int row,int col){
+		//vertically up
+		for(int i=row-1;i>=0;i--){
+			if(board[i][col]=='Q'){
+				return false;
+			}
+		}
+		//daigonally left
+		for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+			if(board[i][j]=='Q'){
+				return false;
+			}
+		}
+		//daigonally right
+		for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+			if(board[i][j]=='Q'){
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void nqueens(char board[][],int row){
+		if(row==board.length){
+			printBoard(board);
+			return;
+		}
+
+		for(int j=0;j<board.length;j++){
+			 if(isSafe(board, row, j)){
+				board[row][j]='Q';
+				nqueens(board, row+1);//function call
+				board[row][j]='x';//backtracking step
+			}
+		}
+	}
+
+	public static void printBoard(char board[][]){
+		System.out.println("-----------------");
+		for(int i=0;i<board.length;i++){
+			for(int j=0;j<board.length;j++){
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String args[]){
+		int n=4;
+		char board[][]=new char[n][n];
+		//initializing the board
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				board[i][j]='x';
+			}
+		}
+		nqueens(board, 0);
+	}
+}
+
+
